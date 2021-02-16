60d9f8dbfe7e47f404561268e9e5ccc4b507f224
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-02-16 14:53:16 +0530

    Welcome updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import '../styles/App.css';
-import Welcome from './Welcome';
+import "../styles/App.css";
+import Welcome from "./Welcome.js";
 
 const App = () => {
   return (
-    <Welcome name="" />
-  )
-}
-
+    <>
+      <Welcome name={"Rahul"} />
+    </>
+  );
+};
 
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
 // write code for Welcome component here
+import React from "react";
+const Welcome = ({ name }) => {
+  return (
+    <>
+      <h1>Hey ! {name}</h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
+};
+export default Welcome;
