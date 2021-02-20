04b17b6cf5be9240e5f61c94e5f60cf10935b83a
Saurabh Shukla <73607833+shukla9045@users.noreply.github.com>
2021-02-21 02:49:46 +0530

    updated

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from "react";
 const Welcome = ({ name }) => {
   return (
     <>
-      <h1>Hey ! {name}</h1>
+      <h1>Hey !{name}</h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
